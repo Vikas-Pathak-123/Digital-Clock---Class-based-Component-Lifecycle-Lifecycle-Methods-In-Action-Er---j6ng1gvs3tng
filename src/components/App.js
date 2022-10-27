6900dd3ae008235162945434a0ff19b7a7640e5f
Vikas Pathak <65367880+Vikas-Pathak-123@users.noreply.github.com>
2022-10-28 01:00:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-import React from 'react'
-import '../styles/App.css';
-const App = () => {
+import "./styles.css";
 
+import React from 'react'
+import { useState, useEffect } from 'react';
+const App = () => {
+  const[Time,setTime]=useState();
+
+  const today = new Date();
+
+  const todaydate = (today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+    return () => clearInterval(interval);
+  });
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time" toLocaleString>
+      {todaydate},
+      {Time}
+      </div>
     </div>
   )
 }
-
-
 export default App;
