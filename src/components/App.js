3bb65839e01abe05f8441826b25baca255cebd18
Vikas Pathak <123vikas.vp79@gmail.com>
2023-03-20 14:09:52 +0530

    Assignment Digital clock

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,14 @@ import "../styles/App.css";
 import React from 'react'
 import { useState, useEffect } from 'react';
 const App = () => {
-  const[Time,setTime]=useState();
-
-  const today = new Date();
-
-  const todaydate = (today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate());
+  const[Time,setTime]=useState("");
 
   useEffect(() => {
     const interval = setInterval(() => {
       setTime(new Date().toLocaleString());
     }, 1000);
     return () => clearInterval(interval);
-  });
+  },[]);
   return (
     <div id="main">
       <div className="date-time">
